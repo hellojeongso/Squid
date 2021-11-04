be333bd90da2bf254a388a7a6a5c01f71027260d
hellojeongso <84715469+hellojeongso@users.noreply.github.com>
2021-11-05 02:00:50 +0900

    responsive-768 update

--- main.js
+++
@@ -1,9 +0,0 @@
-function ScrollIntoView(selector) {
-  const scrollTo = document.querySelector(selector);
-  scrollTo.scrollIntoView({behavior:'smooth'});
-}
-
-const arrowUp = document.querySelector('.arrow-up');
-arrowUp.addEventListener('click', () => {
-  ScrollIntoView('#link_header');
-});
